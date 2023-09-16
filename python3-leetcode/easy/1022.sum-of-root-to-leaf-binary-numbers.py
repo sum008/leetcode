68f4a43bef2214b01587e25508fb847ddc1fb73e
sum008 <sk4641230@gmail.com>
2023-09-16 10:58:54 +0530

    first commit

---
+++ python3-leetcode/easy/1022.sum-of-root-to-leaf-binary-numbers.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=1022 lang=python3
+#
+# [1022] Sum of Root To Leaf Binary Numbers
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+        
+# @lc code=end
+
