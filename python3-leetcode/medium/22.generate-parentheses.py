04c6d83c6265572472c83671b66bbd8f305d807a
sum008 <sk4641230@gmail.com>
2023-09-16 18:22:14 +0530

    accepted solution

--- python3-leetcode/medium/22.generate-parentheses.py
+++ python3-leetcode/medium/22.generate-parentheses.py
@@ -7,10 +7,9 @@
 # @lc code=start
 class Solution:
     def all_paranthesis(self, left_brace, right_brace, brace, cur_str, n, res):
-        # if brace == "(" and cur_str[len(cur_str)-1] == ")":
-        #     return
+        if right_brace > left_brace:
+            return
         final_str = cur_str + brace
-        # cur_left = left_brace
         if left_brace < n:
             self.all_paranthesis(left_brace+1, right_brace,
                                  "(", final_str, n, res)
@@ -20,7 +19,6 @@ class Solution:
         if left_brace+right_brace == n*2:
             if final_str[2*n-1] != "(":
                 res.append(final_str)
-            print(final_str)
             return
 
     def generateParenthesis(self, n: int) -> List[str]:
