62f31e89fcc8a2a9b59f5c5cf206c0cb861411f4
sum008 <sk4641230@gmail.com>
2023-09-17 12:17:10 +0530

    second commit

--- python3-leetcode/medium/40.combination-sum-ii.py
+++ python3-leetcode/medium/40.combination-sum-ii.py
@@ -7,27 +7,32 @@
 # @lc code=start
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        def find_all_combination(cur_list, res, start):
-            if sum(cur_list) > target:
+        def find_all_combination(cur_list, res, start, cur_str, cur_sum):
+            if sum(candidates[start:])+cur_sum < target:
                 return
-            if sum(cur_list) == target:
-                x = cur_list.copy()
-                xx = ''.join(x)
-                if xx not in d:
-                    res.append(x)
-                    d.add(xx)
-                # print(res)
+            if cur_sum == target:
+                # x = cur_list.copy()
+                # print(x)
+                if cur_str not in d:
+                    res.append(cur_list.copy())
+                    d.add(cur_str)
                 return
             while start < len(candidates):
+                if cur_sum+candidates[start] > target:
+                    return
+                # if cur_sum+candidates[start] <= target:
+                final_str = cur_str+str(candidates[start])
+                final_sum = cur_sum + candidates[start]
                 cur_list.append(candidates[start])
-                find_all_combination(cur_list, res, start+1)
+                find_all_combination(
+                    cur_list, res, start+1, final_str, final_sum)
                 cur_list.pop()
                 start += 1
         res = []
         d = set()
         candidates = sorted(candidates)
-        if sum(candidates) >= target:
-            find_all_combination([], res, 0)
+        # if sum(candidates) >= target:
+        find_all_combination([], res, 0, "", 0)
         return res
 
 # @lc code=end
