4e7906ca8a1418fe96f7d16aed47de45cdeaf07c
sum008 <sk4641230@gmail.com>
2023-09-19 17:34:22 +0530

    accepted - optimized further

--- python3-leetcode/medium/47.permutations-ii.py
+++ python3-leetcode/medium/47.permutations-ii.py
@@ -9,23 +9,14 @@ class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         def get_all_permutations(fixed, remaining):
             if len(remaining) == 0:
-                cur_res = ''.join([str(i) for i in fixed])
-                result.append(fixed.copy())
-                record_keep.add(cur_res)
+                result.append(fixed)
+                record_keep.add(tuple(fixed))
                 return
-            start = 0
-            while start < len(remaining):
-                fixed.append(remaining[start])
-                temp = remaining[0:start]+remaining[start+1:]
-                f1 = ''.join([str(i) for i in fixed])
-                f2 = ''.join([str(i) for i in temp])
-                if f1+f2 in record_keep:
-                    start += 1
-                    fixed.pop()
+            for start in range(0, len(remaining)):
+                if tuple(fixed+[remaining[start]] + remaining[0:start]+remaining[start+1:]) in record_keep:
                     continue
-                get_all_permutations(fixed, temp)
-                fixed.pop()
-                start += 1
+                get_all_permutations(
+                    fixed+[remaining[start]], remaining[0:start]+remaining[start+1:])
 
         result = []
         record_keep = set()
