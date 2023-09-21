592a41038d7dbd8c9e435eb2f0a5be3e3219b0b1
sum008 <sk4641230@gmail.com>
2023-09-21 13:40:27 +0530

    accepted - cleaned and further optimized, could be further optimized

--- python3-leetcode/medium/79.word-search.py
+++ python3-leetcode/medium/79.word-search.py
@@ -7,47 +7,33 @@
 # @lc code=start
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def search_word(row, column, n_row, n_col, cur_word_index, cur_word, initial):
-            if row >= n_row or column >= n_col or row < 0 or column < 0 or cur_word_index >= len(word):
-                return
-            if initial == "i":
-                while row < n_row:
-                    column = 0
-                    while column < n_col:
-                        if board[row][column] == word[0]:
-                            search_word(row, column, n_row, n_col,
-                                        cur_word_index, cur_word, "")
-                            if len(res) > 0:
-                                return
-                        column += 1
-                    row += 1
+        def first_word(n_row, n_col):
+            for row in range(0, n_row):
+                for column in range(0, n_col):
+                    if board[row][column] == word[0]:
+                        if search_word(row, column, n_row, n_col,
+                                       0, ""):
+                            return True
+            return False
 
-            else:
-                if board[row][column] != word[cur_word_index]:
-                    return
-                board[row][column] = '1'
-                search_word(row-1, column, n_row, n_col,
-                            cur_word_index+1, cur_word+word[cur_word_index], "")
-                search_word(row+1, column, n_row, n_col,
-                            cur_word_index+1, cur_word+word[cur_word_index], "")
-                search_word(row, column+1, n_row, n_col,
-                            cur_word_index+1, cur_word+word[cur_word_index], "")
-                search_word(row, column-1, n_row, n_col,
-                            cur_word_index+1, cur_word+word[cur_word_index], "")
-                board[row][column] = word[cur_word_index]
-                if cur_word+word[cur_word_index] == word:
-                    res.append(1)
-                    return
+        def search_word(row, column, n_row, n_col, cur_word_index, cur_word):
+            found = False
+            if row >= n_row or column >= n_col or row < 0 or column < 0 or cur_word_index >= len(word):
+                return False
+            if board[row][column] != word[cur_word_index]:
+                return False
+            if cur_word_index == len(word)-1:
+                return True
+            board[row][column] = 0
+            found = search_word(row-1, column, n_row, n_col, cur_word_index+1, cur_word+word[cur_word_index]) or search_word(row+1, column, n_row, n_col, cur_word_index+1, cur_word+word[cur_word_index]) or search_word(
+                row, column+1, n_row, n_col, cur_word_index+1, cur_word+word[cur_word_index]) or search_word(row, column-1, n_row, n_col, cur_word_index+1, cur_word+word[cur_word_index])
+            board[row][column] = word[cur_word_index]
+            return found
 
         col = len(board[0])
         row = len(board)
-        res = []
         if row*col < len(word):
             return False
-        search_word(0, 0, row, col, 0, "", "i")
-        if len(res) > 0:
-            return True
-        return False
-
+        return first_word(row, col)
 
 # @lc code=end
