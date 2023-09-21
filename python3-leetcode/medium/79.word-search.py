87c6278af1383f11732e6b658cadeddc94ba6010
sum008 <sk4641230@gmail.com>
2023-09-21 13:53:03 +0530

    accepted - optimized further

--- python3-leetcode/medium/79.word-search.py
+++ python3-leetcode/medium/79.word-search.py
@@ -6,8 +6,26 @@
 
 # @lc code=start
 class Solution:
+    # find first character of the word in grid
     def exist(self, board: List[List[str]], word: str) -> bool:
         def first_word(n_row, n_col):
+            # check if count of characters(appering in word) in grid is greater than length of word
+            word_map = {}
+            for i in range(0, n_row):
+                for j in range(0, n_col):
+                    if board[i][j] not in word_map:
+                        word_map[board[i][j]] = 1
+                    else:
+                        word_map[board[i][j]] = word_map[board[i][j]]+1
+
+            count = 0
+            for i in (set(word)):
+                if i in word_map:
+                    count += word_map[i]
+            if count < len(word):
+                return False
+
+            # find first character of the word
             for row in range(0, n_row):
                 for column in range(0, n_col):
                     if board[row][column] == word[0]:
@@ -16,6 +34,7 @@ class Solution:
                             return True
             return False
 
+        # find remaining words
         def search_word(row, column, n_row, n_col, cur_word_index, cur_word):
             found = False
             if row >= n_row or column >= n_col or row < 0 or column < 0 or cur_word_index >= len(word):
