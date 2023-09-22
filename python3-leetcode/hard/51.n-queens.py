c8a7777f9fd2c1bd8e1369b1f03f466b21da7167
sum008 <sk4641230@gmail.com>
2023-09-22 11:40:11 +0530

    accepted - little optimization

--- python3-leetcode/hard/51.n-queens.py
+++ python3-leetcode/hard/51.n-queens.py
@@ -9,38 +9,61 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         def driver(config):
             for i in range(0, n):
-                temp = "."*i + "Q" + "."*(n-(i+1))
-                find_valid_boards(0, i, 0, [temp], config)
-                if len(config) > 0:
-                    config.popitem()
+                find_valid_boards(
+                    0, i, 0, ["."*i + "Q" + "."*(n-(i+1))], config)
+
+        # def valid(row, column, conf):
+        #     # check up:
+        #     for i in range(row-1, -1, -1):
+        #         if str(i)+str(column) in conf:
+        #             return False
+        #     # check diagonal left:
+        #     i, j = row-1, column-1
+        #     while i >= 0 and j >= 0:
+        #         if str(i)+str(j) in conf:
+        #             return False
+        #         i -= 1
+        #         j -= 1
+
+        #     # check diagonal right:
+        #     i, j = row-1, column+1
+        #     while i >= 0 and j < n:
+        #         if str(i)+str(j) in conf:
+        #             return False
+        #         i -= 1
+        #         j += 1
+        #     return True
+
+        def valid(row, column, conf):
+
+            for r, c in conf.keys():
+                # check down in conf - increase row only
+                for i in range(r+1, row+1):
+                    if i == row and c == column:
+                        return False
+                # check down-diagonal left
+                i, j = r+1, c-1
+                while i < row+1 and j >= column-1:
+                    if i == row and j == column:
+                        return False
+                    i += 1
+                    j -= 1
+
+                # check down-diagonal right
+                i, j = r+1, c+1
+                while i < row+1 and j < column+1:
+                    if i == row and j == column:
+                        return False
+                    i += 1
+                    j += 1
+            return True
 
         def find_valid_boards(row, column, queen_count, cur_config, conf):
             if row >= n or column >= n or row < 0 or column < 0:
                 return
             # check if valid position
-            # check up:
-            for i in range(row-1, -1, -1):
-                if tuple([i, column]) in conf:
-                    return
-            # check diagonal left:
-            i, j = row-1, column-1
-            while True:
-                if i < 0 or j < 0:
-                    break
-                if tuple([i, j]) in conf:
-                    return
-                i -= 1
-                j -= 1
-
-            # check diagonal right:
-            i, j = row-1, column+1
-            while True:
-                if i < 0 or j >= n:
-                    break
-                if tuple([i, j]) in conf:
-                    return
-                i -= 1
-                j += 1
+            if not valid(row, column, conf):
+                return
 
             conf[tuple([row, column])] = 1
 
@@ -49,11 +72,16 @@ class Solution:
                 res.append(cur_config)
 
             for col in range(0, n):
+
                 find_valid_boards(row+1, col, queen_count+1,
                                   cur_config+["."*col + "Q" + "."*(n-(col+1))], conf)
             conf.popitem()
 
         res = []
+        if n == 2 or n == 3:
+            return res
+        if n == 1:
+            return [["Q"]]
         valid_config = {}
         driver(valid_config)
         return res
