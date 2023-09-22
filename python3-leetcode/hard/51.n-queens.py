74bf6521daf462484a7ec416db3db815218c3992
sum008 <sk4641230@gmail.com>
2023-09-22 18:36:58 +0530

    accepted - optimized further

--- python3-leetcode/hard/51.n-queens.py
+++ python3-leetcode/hard/51.n-queens.py
@@ -14,11 +14,14 @@ class Solution:
 
         def valid(row, column, conf):
             for r, c in conf:
-                if c == column:
-                    return False
-                if abs(r-row) == abs(c-column):
+                if c == column or abs(r-row) == abs(c-column):
                     return False
             return True
+
+        # def check_prev_pos(cur_row, cur_col, prev_row, prev_col):
+        #     if cur_col == prev_col or (cur_row-1 == prev_row and cur_col-1 == prev_col) or (cur_row-1 == prev_row and cur_col+1 == prev_col):
+        #         return False
+        #     return True
 
         def find_valid_boards(row, column, queen_count, cur_config, conf):
             if row >= n or column >= n or row < 0 or column < 0:
@@ -26,15 +29,15 @@ class Solution:
             # check if valid position
             if not valid(row, column, conf):
                 return
-
+            # found valid position
             conf.append([row, column])
-
             # found one valid configuration
             if queen_count == n-1:
                 res.append(cur_config)
-
             for col in range(0, n):
-
+                # check if upcoming position is valid or not, if not then continue
+                if col == column or col-1 == column or col+1 == column:
+                    continue
                 find_valid_boards(row+1, col, queen_count+1,
                                   cur_config+["."*col + "Q" + "."*(n-(col+1))], conf)
             conf.pop()
