10541013aacd055a9c1d708b660f6bccd0d2beb8
sum008 <sk4641230@gmail.com>
2023-11-01 17:57:49 +0530

    accepted

--- python3-leetcode/easy/257.binary-tree-paths.py
+++ python3-leetcode/easy/257.binary-tree-paths.py
@@ -14,22 +14,17 @@
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
         def solve(node, cur_str):
-            x=cur_str
+            x = cur_str
             if node.left != None:
-                x=cur_str+"->"+str(node.left.val)
+                x = cur_str+"->"+str(node.left.val)
                 solve(node.left, x)
-            else:
-                if node.right==None:
-                    res.add(x)
             if node.right != None:
-                x=cur_str+"->"+str(node.right.val)
+                x = cur_str+"->"+str(node.right.val)
                 solve(node.right, x)
-            else:
-                if node.left==None:
-                    res.add(x)
-        res=set()
-        solve(root,str(root.val))
-        return list(res)
-        
-# @lc code=end
+            if node.right == None and node.left == None:
+                res.append(x)
+        res = []
+        solve(root, str(root.val))
+        return res
 
+# @lc code=end
