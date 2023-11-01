05c13da3d0e8d8fe05b0719e392626be5ed612d1
sum008 <sk4641230@gmail.com>
2023-11-01 15:29:31 +0530

    accepted - optimized

--- python3-leetcode/easy/1863.sum-of-all-subset-xor-totals.py
+++ python3-leetcode/easy/1863.sum-of-all-subset-xor-totals.py
@@ -8,17 +8,14 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
         def solve(index, subset):
-            res=0
+            if index>=len(nums):
+                return subset
+            res=subset
             while index<len(nums):
-                res+=solve(index+1, subset+[nums[index]])
-                cur_subset=subset+[nums[index]]
-                x=0
-                for i in range(0,len(cur_subset)):
-                    x = x ^ cur_subset[i]
-                res+=x
+                res+=solve(index+1, subset^nums[index])
                 index+=1
             return res
-        return solve(0,[])
+        return solve(0,0)
         
 # @lc code=end
 
