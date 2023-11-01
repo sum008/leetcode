8d7d99f5b7c50c9ca775b971f317dce7a25536a4
sum008 <sk4641230@gmail.com>
2023-11-01 21:03:53 +0530

    accepted

--- python3-leetcode/easy/401.binary-watch.py
+++ python3-leetcode/easy/401.binary-watch.py
@@ -7,18 +7,15 @@
 # @lc code=start
 class Solution:
     def readBinaryWatch(self, turnedOn: int) -> List[str]:
-        # h = 3, m = 5
         def solve(i, index, s, r, ti, lis, t):
             if i >= ti:
-                if t=="hr":
-                    hr_lis.append(s)
-                else:
-                    mi_lis.append(s)
-                return
+                r.append(s)
+                return r
             while index < len(lis):
                 if (t == "hr" and s+lis[index] <= 11) or (t == "mi" and s+lis[index] <= 59):
-                    solve(i+1, index+1, s+lis[index], r, ti, lis, t)
+                    r = solve(i+1, index+1, s+lis[index], r, ti, lis, t)
                 index += 1
+            return r
 
         h = [8, 4, 2, 1]
         m = [32, 16, 8, 4, 2, 1]
@@ -30,15 +27,14 @@ class Solution:
                 if turnedOn-i < 6 and i < 4:
                     hr = i
                     mi = turnedOn-i
-                    hr_lis=[]
-                    mi_lis=[]
-                    solve(0, 0, 0, [], hr, h, "hr")
-                    solve(0, 0, 0, [], mi, m, "mi")
+                    hr_lis = solve(0, 0, 0, [], hr, h, "hr")
+                    mi_lis = solve(0, 0, 0, [], mi, m, "mi")
                     for h_i in hr_lis:
                         for m_i in mi_lis:
                             res.append(f'{h_i}:{str(m_i).rjust(2,"0")}')
             return res
         else:
             return []
+
 
 # @lc code=end
